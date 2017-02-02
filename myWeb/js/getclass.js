5a8898b8aed89fa135c9d3cd3e67f391cc02cede
kookwe <2287614885@qq.com>
2017-02-02 10:49:13 +0800

    第四次修改

---
+++ myWeb/js/getclass.js
@@ -0,0 +1,21 @@
+var get = {
+    byId: function (id) {
+        return document.getElementById(id)
+    },
+    byClass: function (sClass, oParent) {
+        var aClass = [];
+        var reClass = new RegExp("(^| )" + sClass + "( |$)");
+        var aElem = this.byTagName("*", oParent);
+        for (var i = 0; i < aElem.length; i++) reClass.test(aElem[i].className) && aClass.push(aElem[i]);
+        return aClass
+    },
+    byTagName: function (elem, obj) {
+        return (obj || document).getElementsByTagName(elem)
+    }
+};
+
+/*示例
+ *var oNav = get.byId("nav");
+ var aLi = get.byTagName("li", oNav);
+ var aSubNav = get.byClass("subnav", oNav);
+ * */
